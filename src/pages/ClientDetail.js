3d63f05571919b63d33d84ef6cf9aa44ee63451d
Karem Ceron <karemiceron@gmail.com>
2021-07-24 23:58:52 -0400

    client detail

---
+++ src/pages/ClientDetail.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import { ClientState } from "../clientState";
+
+const ClientDetail = () => {
+  const history = useHistory();
+  const url = history.location.pathname;
+  const [clients, setClients] = useState(ClientState);
+  const [client, setClient] = useState(null);
+  // console.log(history)
+
+  useEffect(() => {
+    const currentClient = clients.filter((ClientState) => ClientState.url === url);
+    setClient(currentClient);
+  }, [clients, url]);
+
+  return (
+    <div>
+      <h1>Client Detail</h1>
+    </div>
+  );
+};
+
+export default ClientDetail;
