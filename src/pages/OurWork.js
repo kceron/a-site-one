f14e6791d7fecd569dd024355df95b9b251c31c2
Karem Ceron <karemiceron@gmail.com>
2021-08-03 14:13:40 -0400

    work scroll

--- src/pages/OurWork.js
+++ src/pages/OurWork.js
@@ -6,9 +6,20 @@ import freeplay from "../img/freeplay-logo.jpg";
 import lawlogo from "../img/law-logo.jpg";
 // Animations
 import { motion } from "framer-motion";
-import { pageAnimation, fade, photoAnim, lineAnim, slider, sliderContainer } from "../animation";
+import {
+  pageAnimation,
+  fade,
+  photoAnim,
+  lineAnim,
+  slider,
+  sliderContainer,
+} from "../animation";
+import { useScroll } from "../components/useScroll";
 
 const OurWork = () => {
+  const [element, controls] = useScroll();
+  const [element2, controls2] = useScroll();
+
   return (
     <StyledWork
       variants={pageAnimation}
@@ -27,18 +38,33 @@ const OurWork = () => {
         <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/tunesat">
           <StyledHide>
-            <motion.img className="client__logo" variants={photoAnim} src={tunesat} alt="Tunesat Logo" />
+            <motion.img
+              className="client__logo"
+              variants={photoAnim}
+              src={tunesat}
+              alt="Tunesat Logo"
+            />
           </StyledHide>
         </Link>
       </StyledClient>
-      <StyledClient>
+      <StyledClient
+        // ref={element}
+        // variants={fade}
+        // animate={controls}
+        // initial="hidden"
+      >
         <h2>Freeplay</h2>
         <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/freeplay">
           <motion.img variants={photoAnim} src={freeplay} alt="Client Logo" />
         </Link>
       </StyledClient>
-      <StyledClient>
+      <StyledClient
+      // ref={element2}
+      // variants={fade}
+      // animate={controls2}
+      // initial="hidden"
+      >
         <h2>Kuhn Law Group</h2>
         <motion.div variants={lineAnim} className="line"></motion.div>
         <Link to="/work/kuhn-law-group">
@@ -53,16 +79,16 @@ const StyledWork = styled(motion.div)`
   min-height: 100vh;
   overflow: hidden;
   padding: 5rem 10rem;
-  background: #DDDDDD;
+  background: #dddddd;
   h2 {
     padding: 1rem 0rem;
   }
 `;
-const StyledClient = styled.div`
+const StyledClient = styled(motion.div)`
   padding-bottom: 10rem;
   .line {
     height: 0.5rem;
-    background: #8F69AD;
+    background: #8f69ad;
     margin-bottom: 3rem;
   }
   img {
