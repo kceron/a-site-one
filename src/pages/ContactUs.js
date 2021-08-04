8145f98e3568dc0f46aa5397aef04efcb5ab6d53
Karem Ceron <karemiceron@gmail.com>
2021-08-03 22:29:04 -0400

    contact animation

--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,20 +1,82 @@
 import React from "react";
 // Animations
 import { motion } from "framer-motion";
-import { pageAnimation } from "../animation";
+import { pageAnimation, titleAnim } from "../animation";
+import styled from "styled-components";
 
 const ContactUs = () => {
   return (
-    <motion.div
+    <StyledContact
       variants={pageAnimation}
       initial="hidden"
       animate="show"
       exit="exit"
-      style={{ background: "#fff" }}
+      style={{ background: "#DDDDDD" }}
     >
-      <h1>Contact Us</h1>
-    </motion.div>
+      <StyledTitle>
+        <Hide>
+          <motion.h2 variants={titleAnim}>Get in touch</motion.h2>
+        </Hide>
+      </StyledTitle>
+      <div>
+        <Hide>
+          <StyledSocial variants={titleAnim}>
+            <Circle />
+            <a href="">LinkedIn</a>
+          </StyledSocial>
+        </Hide>
+        <Hide>
+          <StyledSocial variants={titleAnim}>
+            <Circle />
+            <a href="">Instagram</a>
+          </StyledSocial>
+        </Hide>
+        <Hide>
+          <StyledSocial variants={titleAnim}>
+            <Circle />
+            <a href="">Twitter</a>
+          </StyledSocial>
+        </Hide>
+        <Hide>
+          <StyledSocial variants={titleAnim}>
+            <Circle />
+            <h2>Send us an email</h2>
+          </StyledSocial>
+        </Hide>
+      </div>
+    </StyledContact>
   );
 };
+
+const StyledContact = styled(motion.div)`
+  padding: 5rem 10rem;
+  color: #353535;
+  min-height: 90vh;
+`;
+
+const StyledTitle = styled.div`
+  margin-bottom: 4rem;
+  color: black;
+`;
+
+const Hide = styled.div`
+  overflow: hidden;
+`;
+
+const Circle = styled.div`
+  border-radius: 50%;
+  width: 2rem;
+  height: 2rem;
+  background: #5B2688;
+`;
+
+const StyledSocial = styled(motion.div)`
+  display: flex;
+  align-items: center;
+  h2, a {
+    margin: 2rem;
+    text-decoration: none;
+  }
+`;
 
 export default ContactUs;
